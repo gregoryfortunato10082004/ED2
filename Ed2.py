3e859b029feb5fa6d1d1cac0f9540fdc26cceaa5
Guilherme196928 <196928@upf.br>
2023-12-04 11:18:15 -0300

    terceiro commit

--- Ed2.py
+++ Ed2.py
@@ -10,3 +10,39 @@ class Node:
 # Verifica se o número de parênteses abertos é igual ao número de parênteses fechados
 def check_parentheses(expr):
     return expr.count('(') == expr.count(')')
+
+# Converte a expressão em notação infixa para notação pós-fixa (também conhecida como notação polonesa reversa)
+def parse_expr(expr):
+    tokens = re.findall("[\d.]+|[\w]+|[+/*()-]", expr)
+    prec = {'+':1, '-':1, '*':2, '/':2}
+    ops = []
+    output = []
+    for token in tokens:
+        if token in prec:
+            while ops and ops[-1] != "(" and prec[token] <= prec[ops[-1]]:
+                output.append(ops.pop())
+            ops.append(token)
+        elif token == "(":
+            ops.append(token)
+        elif token == ")":
+            while ops and ops[-1] != "(":
+                output.append(ops.pop())
+            ops.pop()
+        else:
+            output.append(token)
+    while ops:
+        output.append(ops.pop())
+    return output
+
+# Constrói uma árvore binária a partir da expressão em notação pós-fixa
+def build_tree(tokens):
+    stack = []
+    for token in tokens:
+        if token in "+-*/":
+            node = Node(token)
+            node.right = stack.pop()
+            node.left = stack.pop()
+            stack.append(node)
+        else:
+            stack.append(Node(token))
+    return stack[0]
